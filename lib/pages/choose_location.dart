c68782b281a44288f5bf692cf596496c781da7db
Aaryan <aaryanparpyani3@gmail.com>
2022-10-05 20:49:27 +0530

    lab 11 completed

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
-
 class ChooseLocation extends StatefulWidget {
-  const ChooseLocation({Key? key}) : super(key: key);
-
+// const ChooseLocation({Key? key}) : super(key: key);
   @override
   State<ChooseLocation> createState() => _ChooseLocationState();
 }
-
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
 
-
-  var counter = 0;
   @override
   Widget build(BuildContext context) {
-    print("BUILD FUNCTION RUN IN CHOOSE LOCATION Counter = $counter");
+// print('BUILD FUNCTION RUN IN CHOOSE LOCATION...');
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("CHOOSE LOCATION"),
-          backgroundColor: Colors.grey,
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    counter++;
-                  });
-                },
-                child: Text("Counter is $counter"),
-              )
-            ],
-          ),
-        ));
+      backgroundColor: Colors.blueGrey[200],
+// when we come/route from different screen IN APPBAR FLUTTER WILL DEFAULTLY
+// PLACED "BACK ARROW <- .....
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text('CHOOSE LOCATION'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+/*
+body: ElevatedButton(
+onPressed: () {
+setState(() {
+counter += 1;
+});
+},
+child: Text('COUNTER IS : $counter'),
+),
+*/
+    );
   }
 }
