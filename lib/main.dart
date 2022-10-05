ebedbb444a13328c035d3eb90060657716d486bb
Aaryan <aaryanparpyani3@gmail.com>
2022-10-05 20:49:02 +0530

    lab 11 completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,45 @@ import 'package:sdp_lab_10_1_ce085/pages/choose_location.dart';
 import 'package:sdp_lab_10_1_ce085/pages/home.dart';
 import 'package:sdp_lab_10_1_ce085/pages/loading.dart';
 
+/*
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  initialRoute: '/location',
-  routes: {
-    '/': (context) => const Loading(),
-    '/home': (context) => const Home(),
-    '/location': (context) => const ChooseLocation(),
-  },
+// home: Home(),
+routes: { // routes is Map variable with 'key:value' pairs
+// '/about' ....eg.....routes the screen through 'about' widgets
+// '/contact'....
+'/': (context) => Loading(), // base routes....base widget file...main
+file of project
+// above statement will creates error...because it conflict with 'home:
+Home(),
+// because both statements tell flutter to initialize the app from their
+given location
+'/home': (context) => Home(),
+'/location': (context) => ChooseLocation(),
+}
 ));
+*/
+void main() => runApp(MaterialApp(
+// home: Home(),
+// instead of making home: property to make any page to initialize at
+// we can use following code ....
+//     initialRoute: '/home',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
+));
+// void main() => runApp(MaterialApp(
+// // home: Home(),
+// // instead of making home: property to make any page to initialize at
+//
+// // we can use following code ....
+// // initialRoute: '/home',
+//     initialRoute: '/',
+//     routes: {
+//       '/': (context) => Loading(),
+//       '/home': (context) => Home(),
+//       '/location': (context) => ChooseLocation(),
+//     }
+// ));
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,55 +1,76 @@
-import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart';
+// import 'dart:convert';
+// class Loading extends StatefulWidget {
+//   @override
+//   State<Loading> createState() => _LoadingState();
+// }
+// class _LoadingState extends State<Loading> {
+//   void getTime() async {
+// // Make Request for time and receive response
+//     Response response = await
+//     get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Kolkata'));
+//     Map timeData = jsonDecode(response.body);
+//     print(timeData);
+// // Get particular property form timeData...
+//     String dateTime = timeData['datetime'];
+//     String offset = timeData['utc_offset']; //not dst_offset
+//     print(dateTime);
+//     print(offset);
+//     DateTime currentTime = DateTime.parse(dateTime);
+//     print(currentTime);
+// /*
+// DateTime currentOffset = DateTime.parse(offset);
+// print(currentOffset);
+// */
+//     String offsetHours = offset.substring(1,3);
+//     print(offsetHours);
+//     String offsetMinutes = offset.substring(4,6);
+//     print(offsetMinutes);
+//     currentTime = currentTime.add(Duration(minutes:
+//
+//         int.parse(offsetMinutes),hours:int.parse(offsetHours)));
+//     print(currentTime);
+//   }
+//   @override
+//   void initState() {
+//     super.initState();
+//     getTime();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Text('LOADING SCREEN'),
+//     );
+//   }
+// }
+
+/***World time class widget service**/
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../world_time.dart';
-
 class Loading extends StatefulWidget {
-  const Loading({Key? key}) : super(key: key);
-
   @override
   State<Loading> createState() => _LoadingState();
 }
-
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    final response = await http.get(
-        Uri.parse("http://worldtimeapi.org/api/timezone/Asia/Kolkata"));
-    print(response.body);
-
-    Map timeData = jsonDecode(response.body);
-    print(timeData);
-    String dateTime = timeData['datetime'];
-    String offset = timeData['utc_offset'];
-
-    print(dateTime);
-    print(offset);
-    DateTime currentTime = DateTime.parse(dateTime);
-    print(currentTime);
-
-    String offsetHours = offset.substring(1, 3);
-    print(offsetHours);
-    String offsetMinutes = offset.substring(4, 6);
-    print(offsetMinutes);
-
-    currentTime = currentTime.add(Duration(
-        minutes: int.parse(offsetMinutes), hours: int.parse(offsetHours)));
-
-    print(currentTime);
-  }
-
-  String? time = 'LOADING.....';
-
+  // String? time = 'LOADING..........';
   void setWorldTime() async {
-    WordTime timeInstance =
-    WordTime(location: 'kolkata', flag: 'india.png', url: 'Asia/Kolkata');
-    await timeInstance.getTime();
-    setState(() {
-      time = timeInstance.time;
-      print(time);
+    WordTime timeinstance =
+    WordTime(location: 'kolkata',flag: 'india.png',url: 'Asia/Kolkata');
+    await timeinstance.getTime();
+    Navigator.pushNamed(context, '/home', arguments: {
+      'location' : timeinstance.location,
+      'flag' : timeinstance.flag,
+      'time' : timeinstance.time,
     });
+// print(timeinstance.time);
+//     setState(() {
+//       time = timeinstance.time;
+//     });
   }
-
   @override
   void initState() {
     super.initState();
@@ -59,36 +80,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
-        title: const Text('World Time'),
-        centerTitle: true,
-      ),
+      backgroundColor: Colors.deepPurpleAccent,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // IconButton(
-            //     onPressed: () {},
-            //     icon: const Icon(
-            //       Icons.alarm,
-            //       color: Colors.deepPurple,
-            //       size: 40,
-            //     )),
-            const Text("Time for your location",
-                style: TextStyle(fontSize: 20, letterSpacing: 2)),
-            TextButton(
-              child: Text(
-                '$time',
-                style:
-                const TextStyle(wordSpacing: 2, color: Colors.lightGreen),
-              ),
-              onPressed: () {},
-            ),
-          ],
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 50.0,
         ),
       ),
     );
-    ;
   }
 }
